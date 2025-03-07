68996bda6420285847364dd64adf9d33e93300d0
antonioferrigno28 <ferrigno1@hotmail.it>
2025-03-07 18:14:32 +0100

    Seeder/faker Done!

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,12 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        //codice che esegue il seeder
+        $this->call(TrainsTableSeeder::class);
     }
 }
---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Faker\Generator as Faker;
+use App\Models\Train;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        //verrà eseguito ogni volta che lanciamo QUESTO seeder
+        for ($i = 0; $i < 10; $i++) {
+            $newTrain = new Train();
+            $newTrain->company_name = $faker->word();
+            $newTrain->train_departure_station = $faker->city();
+            $newTrain->train_arrival_station = $faker->city();
+            $newTrain->train_departure_time = $faker->dateTimeThisMonth();
+            $newTrain->train_arrival_time = $faker->dateTimeThisMonth();
+            $newTrain->train_code = $faker->randomNumber(5, true);
+            $newTrain->is_food_available = $faker->boolean();
+            $newTrain->number_of_wagon = $faker->numberBetween(5, 12);
+            $newTrain->is_on_time = $faker->boolean();
+            $newTrain->is_cancelled = $faker->boolean();
+
+
+            $newTrain->save();
+        }
+    }
+}
